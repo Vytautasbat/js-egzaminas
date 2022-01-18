0e3df79e1c398fcd6df9a6affb391d62c15d3d30
Vytautas_bat <vyciune@gmail.com>
2022-01-18 21:06:41 +0200

    Calculator

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,15 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+    this.sum = (a, b) => a + b
+    this.subtraction = (a, b) => a - b
+    this.multiplication = (a, b) => a * b
+    this.division = (a, b) => a / b
+}
+const calc = new Calculator()
+console.log(calc.sum(1000, 500))
+console.log(calc.subtraction(1000, 500))
+console.log(calc.multiplication(1000, 500))
+console.log(calc.division(1000, 500))
