42436930323605fafab82aa7d8ea6dc2da87c979
Vytautas_bat <vyciune@gmail.com>
2022-01-18 21:30:08 +0200

    Movies Budget

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,18 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title
+        this.director = director
+        this.budget = budget
+    }
+    wasExpensive() {
+        return this.budget > 100_000_000
+    }
+}
+
+const movie = new Movie('Pirates of the Caribbean: Dead Man\'s Chest', 'Gore Verbinski', 963400000)
+console.log(movie)
+console.log(movie.wasExpensive())
